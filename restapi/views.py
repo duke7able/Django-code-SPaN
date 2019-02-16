10ff9d32d99ee5ff3c8347867b2195ee1c04d51b
duke7able <mehtashreyan041.sm@gmail.com>
2019-02-16 12:10:18 +0530

    modified and removed some debree from code

--- restapi/views.py
+++ restapi/views.py
@@ -10,26 +10,10 @@ import json
 import random
 from lxml.html import fromstring
 
-# Create your views here.
-# Lists all stocks or create a new one
-# stocks/
-#
-# def some(request):
-#     print(request)
-#     print(request.method)
-#     # print(request.data)
-#     return HttpResponseRedirect('/admin')
-
 class codespanview(APIView):
 
     def get(self, request):
-        serializer = StockSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-        stocks = stock.objects.all()
-        serializer = StockSerializer(stocks, many=True)
-        return Response(serializer.data)
+        return Response("Currently no GET queries please")
 
     ###
     #   Params of POST method of stockList
@@ -38,39 +22,6 @@ class codespanview(APIView):
     #
 
     def post(self, request):
-        # obj = request.data
-        # st = stock()
-        # serializer = StockSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     serializer.save()
-        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        # try:
-        # proxies = [
-        # '217.61.107.28:3128',
-        # '151.80.140.233:54566',
-        # '217.61.107.28:3128',
-        # '151.80.140.233:54566',
-        # '46.8.119.71:53281',
-        # '160.16.127.184:3128',
-        # '159.65.0.210:3128',
-        # '217.9.94.145:8080'
-        # ]
-        # def get_proxies():
-        #     url = 'https://free-proxy-list.net/'
-        #     response = requests.get(url)
-        #     parser = fromstring(response.text)
-        #     proxies = []
-        #     for i in parser.xpath('//tbody/tr')[:10]:
-        #         if i.xpath('.//td[7][contains(text(),"yes")]'):
-        #             proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
-        #             proxies.append(proxy)
-        #     return proxies
-        #
-        # proxies = get_proxies()
-        # proxy = random.choice(proxies)
-        # proxy = proxy.split(':')[0]
-        # print(proxy)
         userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
         #Set the headers
         headers = {'User-Agent': userAgent}
@@ -98,38 +49,36 @@ class codespanview(APIView):
             varForCombiningModules.append(test)
             # resultedUrl2.append(test)
         results = []
-        methodUsed = []
-        urls = []
         for url in varForCombiningModules:
             Query = url
             r = requests.get(Query)
             data = r.text
             soup = BeautifulSoup(data, 'html.parser')
-            if(len(soup.find_all('pre'))>0):
+            block = []
+            if(len(soup.find_all('pre'))>1):
                 content = []
                 for c in soup.find_all('pre'):
                     content.append(c.text)
-                results.append(content)
-                methodUsed.append(1)
-                urls.append(url)
-                # print("loop 1")
+                block.append("one")
+                block.append(content)
             elif(soup.pre is not None):
                 if(len(soup.pre.get_text().split('\n'))>0):
-                    results.append(soup.pre.get_text().split('\n'))
-                    methodUsed.append(2)
-                    urls.append(url)
-                    # print("loop 2")
+                    block.append("two")
+                    block.append(soup.pre.get_text().split('\n'))
             elif(len(soup.find_all('code'))>0):
                 content = []
                 for c in soup.find_all('code'):
                     content.append(c.text)
-                results.append(content)
-                methodUsed.append(3)
-                urls.append(url)
-                # print("loop 3")
+                block.append("three")
+                block.append(content)
+
+            # combing it up here
+            if not block:
+                continue
+            block.append(url)
+            results.append(block)
 
         outputBuff  = []
         outputBuff.append(results)
-        outputBuff.append(urls)
         r = json.dumps(outputBuff, sort_keys=True)
         return Response(r)
